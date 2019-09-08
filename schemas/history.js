f75eb3416597b48bcf0764755d3537a48319ff06
lekaren <cpdnql@naver.com>
2019-09-08 20:59:37 +0900

    API 키 발급

---
+++ schemas/history.js
@@ -0,0 +1,15 @@
+const mongoose = required('mongoose');
+
+const { Schema } = mongoose;
+const historySchema = new Schema({
+    query: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.module('History', historySchema);
